57b5e1188542dd756ef204fb55b84af9dd17aadc
sapslaj <saps.laj@gmail.com>
2025-06-18 21:14:49 -0400

    test: implement TODO'd FileLine tests

--- tests/integration/resource_file_line_test.go
+++ tests/integration/resource_file_line_test.go
@@ -56,21 +56,14 @@ EOF
 			},
 		},
 
-		"line addition to new file": {
+		"line addition in existing file": {
 			Create: Operation{
 				Inputs: property.NewMap(map[string]property.Value{
 					"path":   property.New("/etc/default/motd-news"),
 					"line":   property.New("ENABLED=0"),
 					"regexp": property.New("^ENABLED"),
 				}),
-				AssertBeforeCommand: `rm -f /etc/default/motd-news
-cat << EOF | sudo tee /etc/default/motd-news
-# Enable/disable the dynamic MOTD news service
-# This is a useful way to provide dynamic, informative
-# information pertinent to the users and administrators
-# of the local system
-ENABLED=1
-
+				AssertBeforeCommand: `cat << EOF | sudo tee /etc/default/motd-news
 # Configure the source of dynamic MOTD news
 # White space separated list of 0 to many news services
 # For security reasons, these must be https
@@ -96,6 +89,112 @@ EOF
 						"regexp": property.New("^ENABLED"),
 					}),
 					AssertCommand: "grep -q ^ENABLED=0 /etc/default/motd-news",
+				},
+			},
+		},
+
+		"line deletion in existing file": {
+			Create: Operation{
+				Inputs: property.NewMap(map[string]property.Value{
+					"path":   property.New("/etc/default/motd-news"),
+					"regexp": property.New("^ENABLED"),
+					"ensure": property.New("absent"),
+				}),
+				AssertBeforeCommand: `cat << EOF | sudo tee /etc/default/motd-news
+# Enable/disable the dynamic MOTD news service
+# This is a useful way to provide dynamic, informative
+# information pertinent to the users and administrators
+# of the local system
+ENABLED=1
+
+# Configure the source of dynamic MOTD news
+# White space separated list of 0 to many news services
+# For security reasons, these must be https
+# and have a valid certificate
+# Canonical runs a service at motd.ubuntu.com, and you
+# can easily run one too
+URLS=""
+
+# Specify the time in seconds, you're willing to wait for
+# dynamic MOTD news
+# Note that news messages are fetched in the background by
+# a systemd timer, so this should never block boot or login
+WAIT=5
+EOF
+`,
+				AssertCommand: "! grep -q ^ENABLED /etc/default/motd-news",
+			},
+			Updates: []Operation{
+				{
+					Inputs: property.NewMap(map[string]property.Value{
+						"path":   property.New("/etc/default/motd-news"),
+						"regexp": property.New("^ENABLED"),
+						"ensure": property.New("absent"),
+					}),
+					AssertCommand: "! grep -q ^ENABLED /etc/default/motd-news",
+				},
+			},
+		},
+
+		"line addition and updates to new file with create=true": {
+			Create: Operation{
+				Inputs: property.NewMap(map[string]property.Value{
+					"path":   property.New("/fileline"),
+					"line":   property.New("foo bar baz"),
+					"regexp": property.New("^foo"),
+					"create": property.New(true),
+					"triggers": property.New(property.NewMap(map[string]property.Value{
+						"refresh": property.New(property.NewArray([]property.Value{
+							property.New("1"),
+						})),
+					})),
+				}),
+				AssertCommand: "grep -F 'foo bar baz' /fileline",
+			},
+			Updates: []Operation{
+				{
+					Inputs: property.NewMap(map[string]property.Value{
+						"path":   property.New("/fileline"),
+						"line":   property.New("foo bar baz"),
+						"regexp": property.New("^foo"),
+						"create": property.New(true),
+						"triggers": property.New(property.NewMap(map[string]property.Value{
+							"refresh": property.New(property.NewArray([]property.Value{
+								property.New("2"),
+							})),
+						})),
+					}),
+					AssertCommand: "grep -F 'foo bar baz' /fileline",
+					ExpectedDiff: &p.DiffResponse{
+						DeleteBeforeReplace: true,
+						HasChanges:          true,
+						DetailedDiff: map[string]p.PropertyDiff{
+							"triggers": {
+								Kind:      p.Update,
+								InputDiff: true,
+							},
+						},
+					},
+				},
+				{
+					Refresh: true,
+					Inputs: property.NewMap(map[string]property.Value{
+						"path":   property.New("/fileline"),
+						"line":   property.New("foo bar baz"),
+						"regexp": property.New("^foo"),
+						"create": property.New(true),
+						"triggers": property.New(property.NewMap(map[string]property.Value{
+							"refresh": property.New(property.NewArray([]property.Value{
+								property.New("2"),
+							})),
+						})),
+					}),
+					AssertCommand: "grep -F 'foo bar baz' /fileline",
+					ExpectedDiff: &p.DiffResponse{
+						DeleteBeforeReplace: true,
+						HasChanges:          false,
+						DetailedDiff:        map[string]p.PropertyDiff{},
+					},
 				},
 			},
 		},
@@ -162,10 +261,6 @@ EOF
 				},
 			},
 		},
-
-		// TODO: "line addition to new file with create=true"
-		// TODO: "line addition in existing file"
-		// TODO: "line deletion in existing file"
 	}
 
 	for name, tc := range tests {
