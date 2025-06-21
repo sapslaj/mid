48ef4cef7e41e5861b0e923fe3e13d0b227c7ee4
sapslaj <saps.laj@gmail.com>
2025-06-21 09:20:50 -0400

    refactor: use FileStatState for FileStat Function

--- provider/agent/file_stat.go
+++ provider/agent/file_stat.go
@@ -2,8 +2,6 @@ package agent
 
 import (
 	"context"
-	"strconv"
-	"time"
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
@@ -11,8 +9,8 @@ import (
 
 	"github.com/pulumi/pulumi-go-provider/infer"
 	"github.com/sapslaj/mid/agent/rpc"
-	"github.com/sapslaj/mid/pkg/ptr"
 	"github.com/sapslaj/mid/pkg/telemetry"
+	"github.com/sapslaj/mid/provider/types"
 )
 
 type FileStat struct{}
@@ -33,22 +31,7 @@ type FileStatFileMode struct {
 
 type FileStatOutput struct {
 	FileStatInput
-	Path           string            `pulumi:"path"`
-	Exists         bool              `pulumi:"exists"`
-	BaseName       *string           `pulumi:"baseName,optional"`
-	Size           *int64            `pulumi:"size,optional"`
-	FileMode       *FileStatFileMode `pulumi:"fileMode,optional"`
-	ModifiedTime   *string           `pulumi:"modifiedTime,optional"`
-	AccessTime     *string           `pulumi:"accessTime,optional"`
-	CreateTime     *string           `pulumi:"createTime,optional"`
-	Dev            *int              `pulumi:"dev,optional"`
-	Gid            *int              `pulumi:"gid,optional"`
-	GroupName      *string           `pulumi:"groupName,optional"`
-	Inode          *int              `pulumi:"inode,optional"`
-	Nlink          *int              `pulumi:"nlink,optional"`
-	Uid            *int              `pulumi:"uid,optional"`
-	UserName       *string           `pulumi:"userName,optional"`
-	SHA256Checksum *string           `pulumi:"sha256Checksum,optional"`
+	types.FileStatState
 }
 
 func (f FileStat) Invoke(
@@ -74,48 +57,8 @@ func (f FileStat) Invoke(
 	}
 
 	output := FileStatOutput{
-		FileStatInput:  req.Input,
-		Path:           out.Path,
-		Exists:         out.Exists,
-		BaseName:       out.BaseName,
-		Size:           out.Size,
-		GroupName:      out.GroupName,
-		UserName:       out.UserName,
-		SHA256Checksum: out.SHA256Checksum,
-	}
-
-	if out.FileMode != nil {
-		output.FileMode = &FileStatFileMode{
-			IsDir:     out.FileMode.IsDir(),
-			IsRegular: out.FileMode.IsRegular(),
-			Int:       int(*out.FileMode),
-			Octal:     strconv.FormatUint(uint64(*out.FileMode), 8),
-			String:    out.FileMode.String(),
-		}
-	}
-	if out.ModifiedTime != nil {
-		output.ModifiedTime = ptr.Of(out.ModifiedTime.Format(time.RFC3339Nano))
-	}
-	if out.AccessTime != nil {
-		output.AccessTime = ptr.Of(out.AccessTime.Format(time.RFC3339Nano))
-	}
-	if out.CreateTime != nil {
-		output.CreateTime = ptr.Of(out.CreateTime.Format(time.RFC3339Nano))
-	}
-	if out.Dev != nil {
-		output.Dev = ptr.Of(int(*out.Dev))
-	}
-	if out.Gid != nil {
-		output.Gid = ptr.Of(int(*out.Gid))
-	}
-	if out.Inode != nil {
-		output.Inode = ptr.Of(int(*out.Inode))
-	}
-	if out.Nlink != nil {
-		output.Nlink = ptr.Of(int(*out.Nlink))
-	}
-	if out.Uid != nil {
-		output.Uid = ptr.Of(int(*out.Uid))
+		FileStatInput: req.Input,
+		FileStatState: types.FileStatStateFromRPCResult(out),
 	}
 
 	span.SetAttributes(telemetry.OtelJSON("pulumi.output", output))
